eed7de1b72493e5759bd917f4b5e244e11d040c4
Matthew Day <matt@matthewday.net>
2024-07-03 19:22:37 -0400

    include vendor count in vendors query

--- sql/vendors.js
+++ sql/vendors.js
@@ -1,3 +1,16 @@
 export const sqlGetVendors = () => (`
-  SELECT vendor_id, vendor_name FROM vendors;
+  SELECT vendor_id, vendor_name, (
+    SELECT count(*)
+    FROM transactions t
+    WHERE t.vendor_id = v.vendor_id
+  ) as count
+  FROM vendors v
+  ORDER BY v.vendor_name;
 `);
+
+/*
+NOTE
+
+The `ORDER BY` is case sensitive so vendor names that begin with a
+lowercase letter will be at the end of the list.
+*/
