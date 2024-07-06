c564e45a68a6f91f5deecde477230128c7a1b35a
Matthew Day <matt@matthewday.net>
2024-07-06 13:15:12 -0400

    remove vendors and codes from navigation

--- react-ui/src/views/index.tsx
+++ react-ui/src/views/index.tsx
@@ -1,19 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import { CodesPage } from './codes';
 import { PrivateView } from './private/private';
 import { LandingPage } from './landing';
 import { LoginPage } from './login';
 import { LogoutPage } from './logout';
 import { TransactionsPage } from './transactions';
-import { VendorsPage } from './vendors';
 
 export const View = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<LandingPage />} />
-    <Route path="/codes" element={<CodesPage />} />
     <Route path="/login" element={<LoginPage />} />
     <Route path="/logout" element={<LogoutPage />} />
     <Route path="/transactions" element={<TransactionsPage />} />
-    <Route path="/vendors" element={<VendorsPage />} />
   </Routes>
 );
