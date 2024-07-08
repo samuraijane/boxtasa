cf036a432643516a35bde703aec4771fb2ddd82c
Matthew Day <matt@matthewday.net>
2024-07-07 22:07:19 -0400

    include count and totals in query for vendors

--- sql/codes.js
+++ sql/codes.js
@@ -1,3 +1,15 @@
 export const sqlGetCodes = () => (`
-  SELECT * FROM codes;
+  SELECT code_id, code_name, code_description,
+    (
+      SELECT count(*)
+      FROM transactions t
+      WHERE t.code_id = c.code_id
+    ) as count,
+    (
+      SELECT sum(t.amount)
+      FROM transactions t
+      WHERE t.code_id = c.code_id
+    ) as total
+  FROM codes c
+  ORDER BY c.code_name;
 `);
